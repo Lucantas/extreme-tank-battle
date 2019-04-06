83c94c7fdb3c73673baec2628da2e65684deb49e
Lucas <lucas.lucantas38@gmail.com>
2019-04-06 20:18:40 -0300

    updates to tank movement

--- source/Characters/Tank/Player.gd
+++ source/Characters/Tank/Player.gd
@@ -1,4 +1,34 @@
-extends "res://Characters/Tank/Tank.gd"
+extends KinematicBody2D
 
-func control(delta):
+export (int) var speed = 200
+export (float) var rotation_speed = 1.5
+
+var velocity = Vector2()
+var rotation_dir = 0
+var screen_size = 0
+
+func ready():
+	screen_size = get_viewport_rect().size
+
+func get_input():
 	$Barrel.look_at(get_global_mouse_position())
+	screen_size = get_viewport_rect().size
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+	    rotation_dir += 1
+	if Input.is_action_pressed('ui_left'):
+	    rotation_dir -= 1
+	
+	if Input.is_action_pressed('ui_down'):
+	    velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed('ui_up'):
+	    velocity = Vector2(speed, 0).rotated(rotation)
+
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	move_and_slide(velocity)
--- source/Characters/Tank/Tank.gd
+++ source/Characters/Tank/Tank.gd
@@ -1,2 +1,9 @@
 extends KinematicBody2D
 
+export (float) var rotation_speed
+export (int) var max_speed
+export (int) var speed = 200
+
+var velocity = Vector2()
+var alive = true
+var rotation_dir = 0
